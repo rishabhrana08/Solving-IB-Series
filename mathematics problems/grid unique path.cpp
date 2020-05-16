9645a9654c0377a9cdec7808fa9dc9c7167099ae
Rishabh Rana <rishabhrana081020@gmail.com>
2020-05-16 19:05:37 +0530

    maths problem section 3

--- mathematics problems/grid unique path.cpp
+++ mathematics problems/grid unique path.cpp
@@ -1,3 +1,6 @@
+
+//recursive approach
+
 int Solution::uniquePaths(int A, int B)
 {
 if(A==B && A==1){
@@ -7,5 +10,45 @@ if(A==1 || B==1){
 return 1;
 }
 return uniquePaths(A-1,B)+uniquePaths(A,B-1);
+}
+
+
+//combinatrics approach
+
+int Solution::uniquePaths(int A, int B)
+{
+    
+    int m=A;int n=B;
+    // m+n-2 C n-1 = (m+n-2)! / (n-1)! (m-1)! 
+    long long ans = 1;
+    for (int i = n; i < (m + n - 1); i++) {
+        ans *= i;
+        ans /= (i - n + 1);
+    }
+    return (int)ans;
+}
+
+
+// dp solution
+
+int Solution::uniquePaths(int A, int B)
+{
+    int dp[A][B];
+    for(int i=0;i<A;i++)
+    {
+        dp[i][0]=1;
+    }
+    for(int j=0;j<B;j++)
+    {
+        dp[0][j]=1;
+    }
+    for(int i=1;i<A;i++)
+    {
+        for(int j=1;j<B;j++)
+        {
+            dp[i][j]=dp[i-1][j]+dp[i][j-1];
+        }
+    }
+    return dp[A-1][B-1];
 }
 
